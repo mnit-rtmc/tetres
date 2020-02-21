fd729a5acb20bc0c5c1b49b7b6342a729ac1704a
Ethan Beauclaire <ethan.beauclaire@state.mn.us>
2020-02-21 15:58:46 -0600

    Added server files

---
+++ Server/src/global_settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Chongmyung Park (chongmyung.park@gmail.com)'
+
+import os
+import sys
+
+CUR_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(os.path.dirname(CUR_PATH), 'data')
+LIB_PATH = os.path.join(os.path.dirname(CUR_PATH), 'libs')
+CONFIG_FILE_PATH = os.path.join(os.path.dirname(CUR_PATH), 'tetres.conf')
+
+# add path
+sys.path.append(LIB_PATH)
+
+DOWNLOAD_TRAFFIC_DATA_FILES = True
+
+
