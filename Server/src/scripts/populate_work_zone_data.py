99ccf05deda4e7f01fcb33f5040be769ab78f617
asifmahmudumd <mahmu037@d.umn.edu>
2020-09-22 16:20:28 -0500

    SERVER: - work zone 2019 and 2020 excel data added. - special event latest excel data file name updated - populate work zone data script modified

--- Server/src/scripts/populate_work_zone_data.py
+++ Server/src/scripts/populate_work_zone_data.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("Server/src")
 import global_settings
 from pyticas import ticas
@@ -10,28 +11,26 @@ from tetres_data_populator.work_zone_data.work_zone_api_writer import WorkZoneAP
 from tetres_data_populator.work_zone_data.work_zone_excel_reader import WorkZoneExcelReader
 
 if __name__ == "__main__":
-
     ticas.initialize(global_settings.DATA_PATH)
     infra = Infra.get_infra()
 
     base_url = "http://localhost:5000"
-    only_file_names = ["work_zone_2010.xlsx", "work_zone_2011.xlsx", "work_zone_2012.xlsx", "work_zone_2013.xlsx", "work_zone_2014.xlsx"]
-    for only_file_name in only_file_names:
-        work_zone_filename = "Server/src/tetres_data_populator/excel_data/work_zone_data/{}".format(only_file_name)
-        work_zone_excel_reader = WorkZoneExcelReader(filename=work_zone_filename)
+    only_file_name = input("Enter the name of the snow event excel file: ")
+    work_zone_filename = "Server/src/tetres_data_populator/excel_data/work_zone_data/{}".format(only_file_name)
+    work_zone_excel_reader = WorkZoneExcelReader(filename=work_zone_filename)
 
-        # adding work zone group data
-        work_zone_api_writer = WorkZoneAPIWriter(infra=infra)
-        work_zone_api_writer.post_data(excel_reader=work_zone_excel_reader,
-                                       post_url=base_url + api_urls_admin.WZ_GROUP_INSERT,
-                                       list_url=base_url + api_urls_admin.WZ_GROUP_LIST,
-                                       data_type="work_zone_group",
-                                       api_reader_class=WorkZoneAPIReader)
+    # adding work zone group data
+    work_zone_api_writer = WorkZoneAPIWriter(infra=infra)
+    work_zone_api_writer.post_data(excel_reader=work_zone_excel_reader,
+                                   post_url=base_url + api_urls_admin.WZ_GROUP_INSERT,
+                                   list_url=base_url + api_urls_admin.WZ_GROUP_LIST,
+                                   data_type="work_zone_group",
+                                   api_reader_class=WorkZoneAPIReader)
 
-        work_zone_api_writer.post_data(excel_reader=work_zone_excel_reader,
-                                       post_url=base_url + api_urls_admin.WZ_INSERT,
-                                       list_url=base_url + api_urls_admin.WZ_LIST_ALL,
-                                       data_type="work_zone",
-                                       api_reader_class=WorkZoneAPIReader)
+    work_zone_api_writer.post_data(excel_reader=work_zone_excel_reader,
+                                   post_url=base_url + api_urls_admin.WZ_INSERT,
+                                   list_url=base_url + api_urls_admin.WZ_LIST_ALL,
+                                   data_type="work_zone",
+                                   api_reader_class=WorkZoneAPIReader)
 
-        work_zone_api_writer.populate_work_zone_years(api_reader=WorkZoneAPIReader())
+    work_zone_api_writer.populate_work_zone_years(api_reader=WorkZoneAPIReader())
